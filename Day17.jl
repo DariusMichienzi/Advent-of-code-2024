df1619a3e6132f6300c217ef1d396b2500970feb
Darius Michienzi <er19801@bristol.ac.uk>
2024-12-18 10:12:23 +0000

    added code for days 17 and 18

---
+++ Day17.jl
@@ -0,0 +1,70 @@
+f = "/home/er19801/julia_code/Advent-of-code-24/Datafiles/Day17Data.txt"
+lines = readlines(f)
+linesplit = findfirst(x->x=="",lines)
+
+literal = [0,1,2,3]
+regs = append!(literal,parse.(Int,hcat(split.(lines[1:linesplit-1]," ")...)[3,:]))
+program = parse.(Int,split(split.(prod(lines[linesplit+1:end])," ")[2],","))
+
+function outputcode(program,regs)
+    output = []
+    point = 0
+while point<length(program)
+    opcode = program[point+1]
+    operand = program[point+2]
+    combo = regs[operand+1]
+
+if opcode == 0 
+    regs[5] = regs[5] >> combo
+
+elseif opcode == 1
+    regs[6] = xor(regs[6],operand)
+
+elseif opcode == 2
+    regs[6] = mod(combo,8)
+
+elseif opcode == 3 && regs[5] !=0
+    point = operand
+    continue
+elseif opcode == 4
+    regs[6] = xor(regs[6],regs[7])
+
+elseif opcode == 5
+    push!(output,mod(combo,8))
+
+elseif opcode == 6
+    regs[6] = regs[5] >> combo
+
+elseif opcode == 7
+    regs[7] = regs[5] >> combo
+end
+point+=2
+end
+return output
+end
+
+out = outputcode(program,regs)
+answer=""
+for i in out
+    answer *= string(i)*","
+end
+ans1 = chop(answer,tail=1)
+println("part 1 answer = $ans1")
+
+#end of part 1 
+
+answers = []
+test=[(1,0)]
+for (i,a) in test
+    for k in a:a+8
+        if i<17 && outputcode(program,[0,1,2,3,k,0,0]) == reverse(reverse(program)[1:i])
+            push!(test,(i+1, k*8))
+            if i == length(program)
+                push!(answers,k)
+            end
+        end
+    end
+end
+
+ans2 = minimum(answers)
+println("part 2 answer = ",ans2)
---
+++ Day18.jl
@@ -0,0 +1,45 @@
+f = "/home/er19801/julia_code/Advent-of-code-24/Datafiles/Day18Data.txt"
+lines = readlines(f)
+coords = [((parse(Int,line[2]),parse(Int,line[1]))) for line in split.(lines,",")]
+
+size = (70,70)
+memory = fill(0,size.+(1,1))
+
+for i in 1:1024
+    memory[CartesianIndex(coords[i].+(1,1))] = -1
+end
+
+start = (1,1)
+finish = size.+(1,1)
+directions = [(0,1),(1,0),(0,-1),(-1,0)]
+
+function steps(mem)
+    memory = copy(mem)
+    tests = [start]
+    while length(tests) !== 0 
+        current = popfirst!(tests)
+        for d in directions
+            if get(memory,current.+d,NaN) == 0 || get(memory,current.+d,NaN) > get(memory,current,NaN)+1
+                push!(tests,(current.+d))
+                memory[CartesianIndex(current.+d)] = memory[CartesianIndex(current)]+1
+            end
+        end
+    end
+    return memory[CartesianIndex(finish)]
+end
+ans1 = steps(memory)
+println("part 1 answer = ",ans1 )
+
+# end of part 1 
+
+memory2 = copy(memory)
+ans2 = 0
+for c in coords[1025:end]
+    memory2[CartesianIndex(c.+(1,1))] = -1
+    if steps(memory2) == 0 
+        ans2 = reverse(c)
+       break
+    end
+end
+
+println("part 2 answer = ",ans2)
